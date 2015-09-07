51a26d77f6355df4f442107b2c5557be240bcc94
diego <diego@ubuntu.ubuntu-domain>
2015-09-07 00:17:55 -0600

    first

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: diego
+ *
+ * Created on September 6, 2015, 5:46 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
