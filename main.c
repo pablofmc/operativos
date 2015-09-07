2c1f06c24173cf8b1fe4499502525573d66cd688
diego <diego@ubuntu.ubuntu-domain>
2015-09-07 01:27:24 -0600

    first

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#define FILA 8
-#define COLUMNA 8
+#include "matriz.h"
+#include "archivo.h"
 
 void *mifuncion(void *arg) {
 	int *argu;
@@ -21,66 +21,30 @@ void *mifuncion(void *arg) {
 }
 
 int main() {
-	printf("      hola \n \n");
+    printf("      hola \n \n");
 
-	pthread_t tid;
-	int misargs[2];
+    pthread_t tid;
+    int misargs[2];
 
-	void *mifuncion(void *arg);
+    void *mifuncion(void *arg);
 
-	printf("Creando hilo...\n");
-	fflush(stdout);
+    printf("Creando hilo...\n");
+    fflush(stdout);
 
-	misargs[0] = -5;
-	misargs[1] =-6;
+    misargs[0] = -5;
+    misargs[1] =-6;
 
-	pthread_create(&tid, NULL, mifuncion, (void*) misargs);
+    pthread_create(&tid, NULL, mifuncion, (void*) misargs);
 
-	printf("Hilo creado. Esperando su finalizacion..\n");
-	fflush(stdout);
+    printf("Hilo creado. Esperando su finalizacion..\n");
+    fflush(stdout);
 
-	pthread_join(tid, NULL);
-	fflush(stdout);
+    pthread_join(tid, NULL);
+    fflush(stdout);
 
-	int laberinto[FILA][COLUMNA];
-	int i=0, j=0, ch, cont;
-	char as = '*', opc, jugarn;
+    generarMatriz();
+    leerArchivo();
+    imprimirMatriz();
 
-	for(i=0;i<FILA;i++)
-	{
-		for(j=0;j<COLUMNA;j++)
-		{
-			laberinto[i][j] = 0;
-		}
-	}
-
-	i = j = 0;
-
-	FILE *arch;
-
-	//llena la matriz apartir del archivo
-	arch = fopen("archivo.txt", "r");
-
-	while(!feof(arch)) {
-		ch=(int)fgetc(arch);
-		if(ch!='\n') {
-			laberinto[i][j]=ch;
-			j++;
-		} else {
-			j=0;
-			i++;
-		}
-	}
-	fclose(arch);
-
-	//imprime la matriz
-	for(i=0; i<FILA; i++) {
-		for(j=0; j<COLUMNA; j++) {
-			printf("%c", laberinto[i][j]);
-			printf(" ");
-		}
-		printf("\n");
-	}
-
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
