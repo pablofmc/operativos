7b63394c0d77437d0d83e97414783c63ff7b6744
diego <diego@ubuntu.ubuntu-domain>
2015-09-07 01:38:17 -0600

    first

--- matriz.h
+++ matriz.h
@@ -5,9 +5,33 @@
  * Created on September 7, 2015, 12:54 AM
  */
 
+#include "variables.h"
 #ifndef MATRIZ_H
 #define	MATRIZ_H
 
+
+void generarMatriz() {
+    for(i=0;i<FILA;i++)
+    {
+            for(j=0;j<COLUMNA;j++)
+            {
+                    laberinto[i][j] = 0;
+            }
+    }
+
+    i = j = 0;
+}
+
+void imprimirMatriz() {
+    //imprime la matriz
+    for(i=0; i<FILA; i++) {
+            for(j=0; j<COLUMNA; j++) {
+                    printf("%c", laberinto[i][j]);
+                    printf(" ");
+            }
+            printf("\n");
+    }
+}
 
 
 #endif	/* MATRIZ_H */
---
+++ variables.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   variables.h
+ * Author: diego
+ *
+ * Created on September 7, 2015, 1:19 AM
+ */
+
+#ifndef VARIABLES_H
+#define	VARIABLES_H
+#define FILA 8
+#define COLUMNA 8
+
+int i=0, j=0, ch, cont;
+char as = '*', opc, jugarn;
+    
+int laberinto[FILA][COLUMNA];
+FILE *arch;
+
+
+#endif	/* VARIABLES_H */
+
