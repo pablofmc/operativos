52e232c63493e6190651a67257a42559425b2e2d
pablofmc <pablofmc@ubuntu>
2015-09-08 14:16:08 -0700

    Merge origin/master
    Conflicts:
    	archivo.c
    	estructura.h
    	hilo.c
    	hilo.h
    	main.c
    	matriz.c
    	variables.h

--- hilo.h
+++ hilo.h
@@ -1,13 +1,24 @@
+/* 
+ * File:   hilo.h
+ * Author: pablofmc
+ *
+ * Created on September 8, 2015, 2:12 PM
+ */
 
-#ifndef ESTRUCTURA_H
-#define	ESTRUCTURA_H
-#include <stdbool.h>
-struct campo {
-    char valor;
-    bool arriba;
-    bool abajo;
-    bool derecha;
-    bool izquierda;
-};
+#ifndef HILO_H
+#define	HILO_H
 
-#endif	/* ESTRUCTURA_H */
+
+void movex(void *arg, int v);
+void die();
+void movey(void *arg, int v);
+void ways(void *arg);
+void crearHijoArriba(void *arg);
+void crearHijoAbajo(void *arg);
+void crearHijoDerecha(void *arg);
+void crearHijoIzquierda(void *arg);
+void *routine(void *arg);
+void *move(void *arg);
+
+#endif	/* HILO_H */
+
